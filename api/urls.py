7ffe293c500722d6464e2176d8c335ac7adb1acf
Mirjaxon <mirjaxon457@gmail.com>
2024-03-06 12:06:01 +0500

    views

--- api/urls.py
+++ api/urls.py
@@ -3,10 +3,7 @@ from .views import(
      add_product,get_product,
      all_product,name_product,
      price_product,color_product,
-     delete_product,price1_product,
-     brind_product,iendswith_product,
-     istartswith,get_product_id,lt_product,
-     gt_product,lst_models,
+     delete_product,get_product_id,lst_models,
      get_name,update_product,
 )
 
@@ -18,13 +15,7 @@ urlpatterns = [
     path("name/<name>/",name_product),
     path("color/<color>/",color_product),
     path('price/<price>/',price_product),
-    path('price1/<price0><price2>/',price1_product),
-    path('<brind>/all/',brind_product),
-    path('iendswith/<iendswith>/',iendswith_product),
-    path('istartswith/<istartswith>',istartswith),
     path('gte/<int:pk>',get_product_id),
-    path('price_lt/<lt>/',lt_product),
-    path('price_gt/<gt>/',gt_product),
     path('update/<int:pk>',update_product),
     path('models/', lst_models),
     path('models/<str:name>', get_name)
